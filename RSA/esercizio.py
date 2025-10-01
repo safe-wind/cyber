94d11f030b1f62f46d9525a30881de5a4d984a6f
safe-wind <gdl.marius0@gmail.com>
2025-10-01 22:30:27 +0200

    rsa

--- RSA/esercizio.py
+++ RSA/esercizio.py
@@ -1,106 +1,59 @@
-#Private-Key: (2048 bit, 2 primes)
-# modulus:"
-#     00:d3:6e:ac:78:54:28:85:36:90:46:52:5b:71:64\
-#     51:68:06:f4:e3:7d:cb:20:20:89:51:ef:4f:ff:c7:
-#     5e:98:15:d3:76:d4:1f:cb:c1:b8:bb:31:2b:93:d9:
-#     d6:1a:98:eb:06:51:60:42:a1:23:1f:19:d1:a0:d2:
-#     e9:cf:b3:e5:25:8e:b2:1c:dc:74:cf:35:fb:cf:05:
-#     54:a9:b2:52:ef:5b:96:29:0f:86:68:86:9c:a3:48:
-#     bd:71:4d:76:98:ef:5a:02:ef:d4:e5:f6:a8:fe:d6:
-#     27:9f:77:cf:b9:1e:17:bd:d0:a7:49:b0:00:d5:83:
-#     53:b8:ff:10:ed:2d:f0:20:25:b9:e5:93:74:5c:11:
-#     97:6c:c7:09:52:3a:16:26:e1:71:bf:26:d1:78:e5:
-#     5f:88:c2:0c:f2:b7:f4:9d:2e:59:93:c9:9c:52:46:
-#     6c:f1:88:c6:f9:92:19:07:ef:3a:e7:13:55:92:33:
-#     33:8a:62:f7:3b:b2:b8:fb:e2:00:96:95:e4:6c:6c:
-#     a2:eb:03:fa:36:c0:a9:36:59:f6:c8:c7:0a:65:b6:
-#     16:e0:9a:d4:34:e7:5b:d5:f3:33:45:83:75:ad:b4:
-#     2a:f8:ef:8c:2d:e4:cb:44:6a:ba:d0:b9:41:65:15:
-#     a4:0b:43:37:d4:83:bc:63:57:f5:9a:0a:c7:43:80:
-#     f4:a1"
-# publicExponent: 3 (0x3)
-# privateExponent:
-#     08:cf:47:2f:ae:2c:5a:e2:46:02:ee:19:24:b9:83:
-#     64:55:9f:89:7a:93:21:56:b0:63:69:f8:aa:a8:4e:
-#     9b:ab:93:7a:48:d6:a8:7d:67:b2:77:61:d0:d3:be:
-#     41:1b:b4:75:98:b9:58:1c:0c:21:4b:be:11:5e:1f:
-#     13:52:29:8c:3b:47:6b:de:84:dd:f7:95:28:a0:38:
-#     dc:67:6e:1f:4e:7b:97:0b:50:44:5a:f1:31:78:5d:
-#     3a:0d:e4:f1:09:f9:15:74:a8:de:ea:47:0a:9e:41:
-#     a6:a4:fd:fd:0b:eb:a7:e8:b1:a3:12:00:08:e5:78:
-#     d2:5f:f6:09:e1:ea:01:56:d4:55:bb:2a:45:f0:e5:
-#     b3:84:bd:7b:a6:e8:0b:97:01:b1:bc:33:fe:66:e0:
-#     65:6d:fb:dd:cb:86:78:64:c9:9f:46:52:15:0f:8f:
-#     0d:43:ed:4b:fd:94:4c:e1:8a:44:2d:7d:75:db:5e:
-#     01:39:78:81:29:e1:b3:43:dc:fe:13:71:fb:8b:0b:
-#     f4:23:97:15:da:8c:46:c6:42:b9:2c:5c:c7:9d:90:
-#     70:cb:59:70:6c:2f:aa:d1:c9:42:96:75:26:10:05:
-#     02:22:c2:1b:e8:c9:cf:12:99:53:32:13:41:fa:a7:
-#     a0:cc:e3:3b:5c:2f:ac:79:d6:ac:c9:6b:f7:a2:b5:
-#     8b
-# prime1:
-#     00:dd:8b:bf:32:38:18:8f:0d:94:4b:0d:ec:0c:05:
-#     6a:d3:77:1d:55:c3:20:21:5d:24:30:33:b9:31:3f:
-#     c1:4b:f9:2d:d5:88:9e:80:28:72:c0:57:08:11:09:
-#     f0:5a:a7:20:a1:b3:1d:7a:ec:1d:94:03:16:3a:ed:
-#     0c:f7:10:63:49:f7:c2:c7:68:ab:87:59:88:e2:32:
-#     88:a0:8f:19:1f:2c:06:eb:f4:bb:a3:c0:9a:4b:4b:
-#     9b:bf:aa:ac:f4:00:6f:56:c4:e6:9a:83:18:ba:db:
-#     48:8c:8b:48:ce:36:f7:e0:56:43:a8:7d:3a:cb:2f:
-#     1f:09:0b:db:db:07:52:ff:f1
-# prime2:
-#     00:f4:50:48:4b:95:63:7f:8a:c0:fa:ac:a8:49:0b:
-#     4e:75:9e:64:9b:dc:20:34:a9:4e:3f:f3:ea:28:13:
-#     fa:26:ac:04:14:d0:32:83:8a:b7:c7:82:c4:20:c8:
-#     d9:e9:db:ae:f0:d2:84:e8:80:e2:17:1c:af:55:40:
-#     78:51:b4:5f:ea:df:a0:bc:9f:02:48:d0:ba:a0:61:
-#     3b:14:dd:85:38:82:e9:a0:58:ce:cd:ea:28:98:86:
-#     ac:95:5c:bb:86:1b:c4:5c:00:c6:bf:0c:f9:24:ff:
-#     63:ce:78:be:a4:fe:9e:0c:a1:ca:41:c6:a4:79:08:
-#     cc:cc:5b:4c:cd:04:ec:ef:b1
-# exponent1:
-#     00:93:b2:7f:76:d0:10:5f:5e:62:dc:b3:f2:b2:ae:
-#     47:37:a4:be:39:2c:c0:16:3e:18:20:22:7b:76:2a:
-#     80:dd:50:c9:39:05:bf:00:1a:f7:2a:e4:b0:0b:5b:
-#     f5:91:c4:c0:6b:cc:be:51:f2:be:62:ac:b9:7c:9e:
-#     08:a4:b5:97:86:a5:2c:84:f0:72:5a:3b:b0:96:cc:
-#     5b:15:b4:bb:6a:1d:59:f2:a3:27:c2:80:66:dc:dd:
-#     12:7f:c7:1d:f8:00:4a:39:d8:99:bc:57:65:d1:e7:
-#     85:b3:07:85:de:cf:4f:ea:e4:2d:1a:fe:27:32:1f:
-#     6a:06:07:e7:e7:5a:37:55:4b
-# exponent2:
-#     00:a2:e0:30:32:63:97:aa:5c:80:a7:1d:c5:86:07:
-#     89:a3:be:ed:bd:3d:6a:cd:c6:34:2a:a2:9c:1a:b7:
-#     fc:19:c8:02:b8:8a:cc:57:b1:cf:da:57:2d:6b:30:
-#     91:46:92:74:a0:8c:58:9b:00:96:ba:13:1f:8e:2a:
-#     fa:e1:22:ea:9c:95:15:d3:14:ac:30:8b:27:15:96:
-#     27:63:3e:58:d0:57:46:6a:e5:df:33:f1:70:65:af:
-#     1d:b8:e8:7d:04:12:82:e8:00:84:7f:5d:fb:6d:ff:
-#     97:de:fb:29:c3:54:69:5d:c1:31:81:2f:18:50:b0:
-#     88:88:3c:dd:de:03:48:9f:cb
-# coefficient:
-#     00:8b:1f:1d:df:34:8c:e1:fd:7a:0c:b6:6a:c6:de:
-#     ba:22:f8:bf:08:3f:62:54:88:98:31:6f:8c:6b:29:
-#     dc:a8:49:40:6d:4c:77:de:9a:d6:73:e1:15:94:ed:
-#     a6:3a:4a:54:ef:8a:a9:f1:d8:1d:df:08:48:45:0d:
-#     3c:9b:7a:22:d3:80:2c:36:bf:55:14:00:72:90:8e:
-#     55:60:b2:4c:96:95:ec:26:a1:3f:07:94:68:17:cd:
-#     f3:45:97:48:71:10:5c:e6:71:a1:a1:26:4c:7d:b7:
-#     e1:a6:48:df:a9:14:b0:d1:b2:35:b0:65:95:9f:3b:
-#     ee:cc:34:8c:b2:bf:7d:88:24
-from cryptography.hazmat.primitives import serialization
 
-# Carica la chiave privata (PEM)
-with open("FAprivatekey.pem", "rb") as f:
-    private_key = serialization.load_pem_private_key(f.read())
+modulus= "00d36eac78542885369046525b7164516806f4e37dcb20208951ef4fffc75e9815d376d41fcbc1b8bb312b93d9d61a98eb06516042a1231f19d1a0d2e9cfb3e5258eb21cdc74cf35fbcf0554a9b252ef5b96290f8668869ca348bd714d7698ef5a02efd4e5f6a8fed6279f77cfb91e17bdd0a749b000d58353b8ff10ed2df02025b9e593745c11976cc709523a1626e171bf26d178e55f88c20cf2b7f49d2e5993c99c52466cf188c6f9921907ef3ae713559233338a62f73bb2b8fbe2009695e46c6ca2eb03fa36c0a93659f6c8c70a65b616e09ad434e75bd5f333458375adb42af8ef8c2de4cb446abad0b9416515a40b4337d483bc6357f59a0ac74380f4a1"
+publicExponent= "3"#
+privateExponent= "08cf472fae2c5ae24602ee1924b98364559f897a932156b06369f8aaa84e9bab937a48d6a87d67b27761d0d3be411bb47598b9581c0c214bbe115e1f1352298c3b476bde84ddf79528a038dc676e1f4e7b970b50445af131785d3a0de4f109f91574a8deea470a9e41a6a4fdfd0beba7e8b1a3120008e578d25ff609e1ea0156d455bb2a45f0e5b384bd7ba6e80b9701b1bc33fe66e0656dfbddcb867864c99f4652150f8f0d43ed4bfd944ce18a442d7d75db5e0139788129e1b343dcfe1371fb8b0bf4239715da8c46c642b92c5cc79d9070cb59706c2faad1c94296752610050222c21be8c9cf129953321341faa7a0cce33b5c2fac79d6acc96bf7a2b58b"
+prime1= "00dd8bbf3238188f0d944b0dec0c056ad3771d55c320215d243033b9313fc14bf92dd5889e802872c057081109f05aa720a1b31d7aec1d9403163aed0cf7106349f7c2c768ab875988e23288a08f191f2c06ebf4bba3c09a4b4b9bbfaaacf4006f56c4e69a8318badb488c8b48ce36f7e05643a87d3acb2f1f090bdbdb0752fff1"
+prime2= "00f450484b95637f8ac0faaca8490b4e759e649bdc2034a94e3ff3ea2813fa26ac0414d032838ab7c782c420c8d9e9dbaef0d284e880e2171caf55407851b45feadfa0bc9f0248d0baa0613b14dd853882e9a058cecd ea289886ac955cbb861bc45c00c6bf0cf924ff63ce78bea4fe9e0ca1ca41c6a47908cccc5b4ccd04ecefb1"
+exponent1= "0093b27f76d0105f5e62dcb3f2b2ae4737a4be392cc0163e1820227b762a80dd50c93905bf001af72ae4b00b5bf591c4c06bccbe51f2be62acb97c9e08a4b59786a52c84f0725a3bb096cc5b15b4bb6a1d59f2a327c28066dcdd127fc71df8004a39d899bc5765d1e785b30785decf4feae42d1afe27321f6a0607e7e75a37554b"
+exponent2= "00a2e030326397aa5c80a71dc5860789a3beedbd3d6acd c6342aa29c1ab7fc19c802b88acc57b1cfda572d6b3091469274a08c589b0096ba131f8e2afae122ea9c9515d314ac308b27159627633e58d057466ae5df33f17065af1db8e87d041282e800847f5dfb6dff97defb29c354695dc131812f1850b088883cddde03489fcb"
+coefficient= "008b1f1ddf348ce1fd7a0cb66ac6deba22f8bf083f62548898316f8c6b29dca849406d4c77de9ad673e11594eda63a4a54ef8aa9f1d81ddf0848450d3c9b7a22d3802c36bf55140072908e5560b24c9695ec26a13f07946817cdf345974871105ce671a1a1264c7db7e1a648dfa914b0d1b235b065959f3beecc348cb2bf7d8824"
 
-# Estrai parametri
-numbers = private_key.private_numbers()
+#RSA NON LAVORA CON LETTERE MA SOLO CON NUMERI INTERI
 
-n = numbers.public_numbers.n
-e = numbers.public_numbers.e
-d = numbers.d
+def clean_hex_to_int(clean_h:str) -> int:
 
-print("n =", n)
-print("e =", e)
-print("d =", d)
+    return int(clean_h,16)
+
+# converto il numero hex in int
+n =clean_hex_to_int(modulus)
+e =publicExponent
+d =clean_hex_to_int(privateExponent)
+
+print("n=",n)
+print("e=",n)
+print("d=",n)
+
+msg = "CIAO"
+esponente_bytes = len(msg)
+print("esp_bytes=",esponente_bytes)
+# converto il messaggio in numero intero
+
+msg_bytes = list(msg.encode())
+print("msg_bytes=",msg_bytes)
+
+# il messaggio all'inizio non c'è quindi è 0
+M = 0
+
+for b in msg_bytes:
+    M = M*256+b
+
+print("messaggio cifrato=", M)
+
+bytes_list = []
+
+while M > 0:
+    b=M%256
+    bytes_list.insert(0,b)
+    M=M//256
+
+msg_back= bytes(bytes_list).decode()
+
+print("messaggio decifrato=", msg_back)
+
+
+
+
+
+
+
+
